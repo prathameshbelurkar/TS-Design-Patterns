38b0117973f623ebde9242945a03f7fd8525f258
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-05 16:04:45 +0530

    add events

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -130,6 +130,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -138,6 +139,12 @@ function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   };
 
   return User;
@@ -157,11 +164,8 @@ var user = new User_1.User({
   name: "myname",
   age: 20
 });
-user.set({
-  name: "newname"
-});
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.on("change", function () {});
+console.log(user);
 },{"./models/User":"src/models/User.ts"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { User } from "./models/User";
 
 const user = new User({ name: "myname", age: 20 });
 
-user.set({ name: "newname" });
+user.on("change", () => {});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
