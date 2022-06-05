12d960e42fc0771711f3a121e9ec23977b9f331f
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-05 15:40:39 +0530

    add optional parameters to set method

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136,6 +136,10 @@ function () {
     return this.data[propName];
   };
 
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
   return User;
 }();
 
@@ -152,6 +156,9 @@ var User_1 = require("./models/User");
 var user = new User_1.User({
   name: "myname",
   age: 20
+});
+user.set({
+  name: "newname"
 });
 console.log(user.get("name"));
 console.log(user.get("age"));
