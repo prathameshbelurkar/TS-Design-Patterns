a77e65a21b7c0fb0e0b446816f699096f97798c3
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-05 16:41:23 +0530

    add rest api

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { User } from "./models/User";
+import axios from "axios";
 
-const user = new User({ name: "myname", age: 20 });
-
-user.on("change", () => {
-  console.log("change #1");
-});
-user.on("change", () => {
-  console.log("change #3");
-});
-user.on("save", () => {
-  console.log("Save was triggered");
-});
-
-user.trigger("saveddsanasnaj");
+axios.get("http://localhost:3000/users/1");
